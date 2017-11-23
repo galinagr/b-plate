c4d3d9b6cf340c20b8a812b8b762734f3fa4bb75
Galyna_Gryaznova <Galyna_Gryaznova@epam.com>
2017-11-23 02:44:20 +0200

    Init commit

---
+++ src/store/configureStore.js
@@ -0,0 +1,20 @@
+import  { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import expensesReducer from '../reducers/expenses';
+import filtersReducer from '../reducers/filters';
+import authReducer from '../reducers/auth';
+
+const composeEnhansers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default () => {
+    const store = createStore(
+        combineReducers({
+            expenses: expensesReducer,
+            filters: filtersReducer,
+            auth: authReducer
+        }),
+        composeEnhansers(applyMiddleware(thunk))
+       // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+    return store;
+};
